284f66e04fce808aae15a1cb0d78f19968be21a0
hamsoon22 <dpdms040@gmail.com>
2025-07-07 15:20:37 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import BreakEvenCalculator from './BreakEvenCalculator'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-10">
+      <BreakEvenCalculator />
+    </div>
+  );
+}
+
+export default App;
